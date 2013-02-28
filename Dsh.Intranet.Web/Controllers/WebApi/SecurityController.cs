89ab4f5b76888b18bab9a15058c37aa83dd67068
Jose Velez <jose.velez-colon@msh.dmh.ca.gov>
2013-02-28 15:26:46 -0800

    Controllers Addition

---
+++ Dsh.Intranet.Web/Controllers/WebApi/SecurityController.cs
@@ -0,0 +1,75 @@
+﻿using DSH.Intranet.Data.Contracts;
+using DSH.Intranet.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+/// <summary>
+/// Chad Corrin, 2/22/13 - This is just a placeholder controller as an example of how I like to layout controller methods
+/// </summary>
+namespace DSH.Intranet.Web.Controllers
+{
+    public class SecurityController : ApiControllerBase
+    {
+        public SecurityController(IUnitOfWork uow)
+        {
+            Uow = uow;
+        }
+
+        public SecurityController() { }
+
+        // Get all records
+        // GET /api/security
+        public IEnumerable<Security> Get()
+        {
+            return Uow.Security.GetAll().OrderBy(s => s.SecurityID);
+        }
+
+        // Get a single record by id
+        // GET /api/security/id
+        public Security Get(int id)
+        {
+            var record = Uow.Security.GetById(id);
+            if (record != null) return record;
+            throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.NotFound));
+        }
+
+        // Create a new record
+        // POST /api/security
+        public HttpResponseMessage Post(Security security)
+        {
+            Uow.Security.Add(security);
+            Uow.Commit();
+
+            var response = Request.CreateResponse(HttpStatusCode.Created, security);
+
+            // Compose location header that tells how to get this session
+            // e.g. ~/api/security/id
+            response.Headers.Location =
+                new Uri(Url.Link(WebApiConfig.ControllerAndId, new { id = security.SecurityID }));
+
+            return response;
+        }
+
+        // Update existing record
+        // PUT /api/security
+        public HttpResponseMessage Put(Security security)
+        {
+            Uow.Security.Update(security);
+            Uow.Commit();
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+
+        // Delete existing record
+        // DELETE /api/security/id
+        public HttpResponseMessage Delete(int id)
+        {
+            Uow.Security.Delete(id);
+            Uow.Commit();
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ Dsh.Intranet.Web/Controllers/WebApi/UtilityController.cs
@@ -0,0 +1,51 @@
+﻿using DSH.Intranet.Data.Contracts;
+using DSH.Intranet.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+
+/// <summary>
+/// Chad Corrin, 2/22/13 - This is just a generic utility controller I use for MISC methods that dont belong to a model object
+/// </summary>
+namespace DSH.Intranet.Web.Controllers
+{
+    public class UtilityController : ApiControllerBase
+    {
+        public UtilityController(IUnitOfWork uow)
+        {
+            Uow = uow;
+        }
+
+        public UtilityController() { }
+
+        // Create a new record
+        // POST /api/utility/sendmail
+        //public HttpResponseMessage SendMail(Contact mailMessage)
+        //{
+        //    // Send email
+        //    MailMessage mMailMessage = new MailMessage();
+        //    mMailMessage.From = new MailAddress("contact@yourdomain.com");
+        //    mMailMessage.To.Add("lawn.and.so.on@gmail.com");
+        //    //mMailMessage.To.Add(new MailAddress("myaddress@mydomain.com"));
+        //    mMailMessage.Subject = "New website message from: " + mailMessage.From;
+        //    mMailMessage.Body = "New website message from: " + mailMessage.FromEmail + "<br/>" + mailMessage.Message;
+        //    mMailMessage.IsBodyHtml = true;
+        //    mMailMessage.Priority = MailPriority.Normal;
+        //    SmtpClient mSmtpClient = new SmtpClient();
+        //    NetworkCredential basicCredential = new NetworkCredential("EMAIL", "PASSWORD");
+        //    mSmtpClient.Host = "smtp.yourserver.net";
+        //    mSmtpClient.Port = 25;
+        //    mSmtpClient.EnableSsl = false;
+        //    mSmtpClient.UseDefaultCredentials = false;
+        //    mSmtpClient.Credentials = basicCredential;
+        //    mSmtpClient.Send(mMailMessage);
+
+        //    var response = Request.CreateResponse(HttpStatusCode.Created, mailMessage);
+        //    return response;
+        //}
+    }
+}
