d48e3c2069aa28ce1472fc544f55d5d0b9ffb3bb
Jose Velez <jose.velez-colon@msh.dmh.ca.gov>
2013-03-01 15:10:21 -0800

    MenuAdmin

---
+++ Dsh.Intranet.Web/Areas/Atascadero/AtascaderoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DSH.Intranet.Web.Areas.Atascadero
+{
+    public class AtascaderoAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Atascadero";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Atascadero_default",
+                "Atascadero/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Dsh.Intranet.Web/Areas/MenuAdmin/MenuAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DSH.Intranet.Web.Areas.MenuAdmin
+{
+    public class MenuAdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "MenuAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "MenuAdmin_default",
+                "MenuAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
