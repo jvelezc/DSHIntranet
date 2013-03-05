7f542b62b6215bc0b54c1de97229ed58767d9366
Jose Velez <jose.velez-colon@msh.dmh.ca.gov>
2013-03-05 10:24:19 -0800

    Added an error handler for 404 pages.

---
+++ Dsh.Intranet.Web/Areas/ErrorHandler/Controllers/ErrorHomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSH.Intranet.Web.Areas.ErrorHandler.Controllers
+{
+    public class ErrorHomeController : Controller
+    {
+        //
+        // GET: /ErrorHandler/ErrorHome/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Dsh.Intranet.Web/Areas/ErrorHandler/ErrorHandlerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DSH.Intranet.Web.Areas.ErrorHandler
+{
+    public class ErrorHandlerAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "ErrorHandler";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ErrorHandler_default",
+                "ErrorHandler/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
